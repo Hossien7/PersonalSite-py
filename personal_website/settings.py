7cf61ad58d54d570d89567d510d419ec3a9a9680
Hossein <hossein.tajary@gmail.com>
2025-01-05 19:13:17 +0330

    StaticProjectConvertedToDynamic

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts_app',
+    'projects_app.apps.ProjectsAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- personal_website/views.py
+++ personal_website/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from projects_app.models import projects
+
+project = projects.objects.all()
 
 
 def home(request):
-    return render(request, 'index.html', context={})
+    return render(request, 'index.html', context={'projects': project})
